0eae349a0e89fa25a3c704debffcc1d5a2341ec5
Brian Merchant <bzm3r@proton.me>
2023-12-27 15:38:58 -0800

    sketching out the initial structure leads one to realize that rse really is a project that should be combining usage of existing cargo infrastructure and capabilities

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,9 @@
 # TODO
 
+## Important
+* Create a NixOS module for actually adding in RSE.
+* Allow registry of "known manifests", which the user can simply instantiate by name rather than having to always write out a complete `cargo` manifest within a single-page rust script.
+
+## Stretch Goals
+* allow passing in an additional "known_scripts" registry, to non-declaratively (as far as NixOS configuration is concerned) expand the scripts known to RSE?
 * support input script from `stdin`?
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,129 @@
+use std::{borrow::Cow, collections::HashMap, path::PathBuf};
+
 use bpaf::{Bpaf, Parser};
+use url::Url;
 use xshell::{cmd, Shell};
 
 // bpaf docs: https://docs.rs/bpaf/latest/bpaf/index.html
 // xshell docs: https://docs.rs/xshell/latest/xshell/index.html
 
+#[doc(hidden)]
+macro_rules! doc_warn_impermanence {
+    () => {
+        "\n**Warning:** this does not add the script to `/nix/store/rse/declared_scripts` and you are at risk of losing this information upon a system rebuilds! To declaratively add a script, you must list the scripts's URL in the `declaredScripts` option for the scripting environment's NixOS module."
+    }
+}
+
 enum Script {
-    CargoProject,
-    SingleFile,
-    // TODO: support input from StdIn?
-    // Stdin,
+    /// Name of a script that is assumed to already exist either in:
+    ///     * already built and cached ("instantiated") as a directory in the
+    ///     scripting environment's cache.
+    ///     * associated with an uninstantiated URL in the `known_scripts` file,
+    ///     stored in the `/nix/store` location for this scripting environment.
+    Name(String),
+    /// URL of a script in the form of a traditional `cargo` repository.
+    #[doc = doc_warn_impermanence!()]
+    Project(Url),
+    /// URL of a script in the form of a single file. Unlike a traditional cargo
+    /// project, a single file script has its `cargo` manifest (traditionally
+    /// within a `Cargo.toml` file) included as a module level ("inner block")
+    /// documentation comment of the form:
+    /// ```Rust
+    /// //! ```cargo
+    /// //! # no need for `[package]` level keys such as `name`, `version`, `edition`, etc.
+    /// //! [dependencies]
+    /// //! # list dependencies out as usual
+    /// //! ```
+    /// //! A Rust single-file script is invalid if it does not begin with some module level cargo You can then write further docs here and the
+    /// // actual code of the script begins here.
+    /// ```
+    ///
+    /// The above format was chosen in order to make compliance with the current
+    /// decisions around the unstable feature [`cargo
+    /// script`](https://doc.rust-lang.org/nightly/cargo/reference/unstable.html#script) ([RFC#3502](https://github.com/rust-lang/rfcs/pull/3502))
+    /// as smooth as possible. We list out the manifest using inner line
+    /// comments for now, due to lack of [`rust-analyzer`](https://github.com/rust-lang/rust-analyzer/issues/15318) support for now.
+    #[doc = doc_warn_impermanence!()]
+    SingleFile(Url),
+}
+
+pub trait ScriptOutput {}
+
+pub struct ScriptUrl {
+    /// Note how `cargo-script` plans to store local instances of a cached
+    /// script: https://github.com/rust-lang/cargo/issues/12207#issuecomment-1776089794
+    instance: Option<PathBuf>,
+    /// A [`Url`] to the source code of the script.
+    source: Url,
+}
+
+/// This should use: https://doc.rust-lang.org/cargo/reference/registries.html
+/// Should use `cargo`'s existing features as much as possible, to minimize
+/// effort required to build NixOS support *into* `cargo script`.
+///
+/// 0) https://doc.rust-lang.org/cargo/reference/registries.html#registries
+/// 1) https://doc.rust-lang.org/cargo/reference/running-a-registry.html
+///     * in particular, see: https://github.com/rust-lang/cargo/wiki/Third-party-registries
+pub struct Registry {
+    table: HashMap<String, ScriptUrl>,
+}
+
+impl Registry {
+    fn get(&self, name: &str) -> Option<bool> {
+        unimplemented!()
+    }
+}
+
+pub struct ScriptName<'a>(Cow<'a, str>);
+
+pub enum Status {
+    Declared { instantiated: bool },
+    Instantiated { known: bool },
+}
+
+impl Registry {
+    ///
+    pub fn status<'a, HasName: Into<ScriptName<'a>>>(
+        &self,
+        name: HasName,
+    ) -> Status {
+        unimplemented!()
+    }
+
+    pub fn instantiated<'a, HasName: Into<ScriptName<'a>>>(
+        &self,
+        name: HasName,
+    ) -> bool {
+        unimplemented!()
+    }
+}
+
+pub struct ScriptEnv {
+    /// Path to where the environments dynamic library and script cache are stored.
+    /// Default value: $CARGO_HOME.
+    path: PathBuf,
+    /// Map from the name of a script, to its URL.
+    registry: Registry,
+}
+
+impl Default for ScriptEnv {
+    fn default() -> Self {
+        unimplemented!()
+    }
+}
+
+impl ScriptEnv {
+    pub fn is_instantiated(&self, script: &Script) {}
+    pub fn run<O: ScriptOutput>(&self, script: &Script) -> anyhow::Result<O> {
+        if let Some(url) = self.registry.get(script.name()) {}
+        unimplemented!()
+    }
+}
+
+impl Script {
+    fn name(&self) -> &str {
+        unimplemented!()
+    }
 }
 
 /// CLI for the Rust Script Environment.
